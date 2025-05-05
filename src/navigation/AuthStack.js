eeec30e0ca011b8e7cd71f182fb266d05e9af8a0
duongbill <billduongg@gmail.com>
2025-05-05 22:58:51 +0700

    them splashscreen

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from '../screens/auth/LoginScreen';
-import RegisterScreen from '../screens/auth/RegisterScreen';
-import CustomerTabNavigator from './CustomerTabNavigator';
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "../screens/auth/LoginScreen";
+import RegisterScreen from "../screens/auth/RegisterScreen";
+import SplashScreen from "../screens/auth/SplashScreen";
+import CustomerTabNavigator from "./CustomerTabNavigator";
 
 const Stack = createNativeStackNavigator();
 
 export default function AuthStack() {
   return (
-    <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+    <Stack.Navigator
+      initialRouteName="Splash"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="Splash" component={SplashScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Register" component={RegisterScreen} />
       <Stack.Screen name="CustomerTab" component={CustomerTabNavigator} />
